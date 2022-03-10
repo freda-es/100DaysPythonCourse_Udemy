86e35f7ccdae768e40508fbbf461a4b9565c4559
FredaDyrkaj <86465451+freda-es@users.noreply.github.com>
2022-03-10 14:20:28 +0100

    Update main.py

--- Udemy_Days/Day22/main.py
+++ Udemy_Days/Day22/main.py
@@ -4,11 +4,11 @@ from screen import MyScreen
 
 #setup the screen
 screen = Screen()
-# screen.setup(width=800, height=600)
-# screen.bgcolor("black")
-# screen.title("PONG GAME")
-# screen.tracer(0)# the tracer is off, the screen doesn't show anything, blackscreen
-# my_screen = MyScreen()
+screen.setup(width=800, height=600)
+screen.bgcolor("black")
+screen.title("PONG GAME")
+screen.tracer(0)# the tracer is off, the screen doesn't show anything, blackscreen
+my_screen = MyScreen()
 tim = Turtle()
 tim.pendown()
 tim.shape("square")
