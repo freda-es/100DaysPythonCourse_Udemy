bd8ad7769132fe71361f340ed3d4d199fbe702aa
FredaDyrkaj <86465451+freda-es@users.noreply.github.com>
2022-03-29 10:59:14 +0200

    Finished-Day25

--- Udemy_Days/Day25/USA-States-quiz/main.py
+++ Udemy_Days/Day25/USA-States-quiz/main.py
@@ -2,20 +2,45 @@ import turtle
 import pandas
 from states_name import State_OnMap
 
-# setting up the screen with the map of usa and the littel 
+# setting up the screen with the map of usa and the little 
 # prompt where the user will enter his guessing state
 screen = turtle.Screen()
 screen.title("U.S. State Game")
 image = "C:\\Users\\esoft\\Documents\\GitHub\\100DaysPythonCourse_Udemy\\Udemy_Days\\Day25\\USA-States-quiz\\blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
-states = pandas.read_csv("Udemy_Days\\Day25\\USA-States-quiz\\50_states.csv")
-state_n = State_OnMap()
-game_is_on = True
 
-# while game_is_on:   
-answer_state = turtle.textinput(title="Guess the State", prompt="What's another state's name?").lower()
-the_state = states['state']
-for statess in the_state:
-    if answer_state == statess.lower():
-        state_n(answer_state)
+#read the states file from csv
+states = pandas.read_csv("Udemy_Days\\Day25\\USA-States-quiz\\50_states.csv")
+
+#declare a liste where to put the correct answers to loop through later on
+correct_guess = []
+
+#tha game will continue until the player guesses all the states
+while len(correct_guess) <= 50: 
+    #the textinput for the player    
+    answer_state = turtle.textinput(title=f"{len(correct_guess)}/50 States correct", prompt="What's another state's name?").title()
+    
+    #from our data frame with the states and coordinates extract only the column state and save it as a list
+    the_state = states['state'].to_list()
+    
+    #if the player wants to quit the game
+    if answer_state == "Exit":
+        #with this loop we remove from the original list of states the correct ones that the player has written
+        for correctgues in correct_guess:
+            the_state.remove(correctgues)
+        #create a dataframe with this list and then save it as ea csv file    
+        data_states = pandas.DataFrame(the_state)
+        data_states.to_csv("Udemy_Days\\Day25\\USA-States-quiz\\states_to_learn.csv")
+        break
+    
+    #the control if the answer is correct or no (going through the states and control if the answer's there)
+    if answer_state in the_state: 
+        #the method to extract the value of the two columns x,y fro the state guessed correct            
+        x_post = states.loc[states['state'] == answer_state]['x'].values
+        y_post = states.loc[states['state'] == answer_state]['y'].values
+        #the turtle that makse the procedure of writing the state over the map in its position
+        State_OnMap((x_post[0],y_post[0]),answer_state)
+        correct_guess.append(answer_state)
+    
+    
--- Udemy_Days/Day25/USA-States-quiz/states_name.py
+++ Udemy_Days/Day25/USA-States-quiz/states_name.py
@@ -1,11 +1,14 @@
 from turtle import Turtle
+ALIGNMENT = "Center"
+FONT = ('Arial', 7, 'bold')
+
 
 class State_OnMap(Turtle):
-    def __init__(self):
-        self.position = (0,0)
-    
-    def state_name(self,answer_right):
-        state_name = Turtle()
-        state_name.penup() 
-        state_name.write(f"{answer_right}")        
-        state_name.goto(self.position) 
+    def __init__(self, position, answer_right):
+        super().__init__()
+        self.hideturtle()
+        self.color("black")
+        self.penup()
+        self.goto(position) 
+        self.write(f"{answer_right}", align=ALIGNMENT, font=FONT)   
+        
